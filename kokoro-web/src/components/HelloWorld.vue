957474e07b4ff22213eac4fc7b0e9b3fb184390d
K Farmer <kfarmer@proton.com>
2025-03-30 17:16:33 -0400

    test

--- kokoro-web/src/components/HelloWorld.vue
+++ kokoro-web/src/components/HelloWorld.vue
@@ -18,13 +18,10 @@ audio.save("audio.wav");
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      Kokoro example
     </p>
   </div>
 
